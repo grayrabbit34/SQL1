59031553b0ada756eedb702234473c1d1b94fe59
BURAK DÜLGEROĞLU <dulgerogluburak04@gmail.com>
2025-06-06 12:23:45 +0700

    Odev1.sql dosyası güncellendi

--- Odev1.sql
+++ Odev1.sql
@@ -6,4 +6,5 @@
 
 --SELECT last_name FROM customer WHERE first_name = 'Mary';
 
---SELECT * FROM film WHERE length <=50 AND (rental_rate = 2.99 OR rental_rate = 4.99); 
+--SELECT * FROM film WHERE length <=50 AND (rental_rate != 2.99 OR rental_rate != 4.99); 
+
